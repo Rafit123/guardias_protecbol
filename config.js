03f319ef8e4e094c00baf9a063ea17b0af451e7a
Rafit123 <156668829+Rafit123@users.noreply.github.com>
2026-05-07 21:26:50 -0400

    Add global Supabase configuration

---
+++ config.js
@@ -0,0 +1,3 @@
+// Configuración Global de Supabase para PROTECBOL
+const SUPABASE_URL = 'https://ydziovjsqimzyhgpvzwo.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_evZRp02eu_NZB-JKD4xZFA_ecOYHMz3';
